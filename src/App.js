2f35f99eb9985e79429a4a0e2e910f91b71be3db
Vit Onischenko <slonik0775@gmail.com>
2022-06-20 19:48:03 +0300

    dd

--- src/App.js
+++ src/App.js
@@ -4,18 +4,28 @@ import Dialogs from './components/Dialogs/Dialogs';
 import Header from './components/Header/Header';
 import Navbar from './components/Navbar/Navbar';
 import Profile from './components/Profile/Profile';
+import {BrowserRouter, Routes, Route} from "react-router-dom";
 
-function App() {
+
+
+function App(props) {
   return (
+    <BrowserRouter>
+
     <div className="app-wrapper">
   <Header/>
 
   <Navbar/>
- {/* <Profile/> */}
   <div className='app-wrapper-content'> 
-    <Dialogs/>
+  <Routes>
+                 <Route path="/dialogs" element= {<Dialogs/>}/>
+                 <Route path="/profile" element={<Profile/>}/>
+             </Routes>
+
   </div>
     </div>
+    </BrowserRouter>
+
   );
 }
 
